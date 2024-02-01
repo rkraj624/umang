a3c3fe486e2d5a7ac36fc94c348f4b4e9197e25f
Ravi <ravi@applicatetechnology.com>
2024-02-01 10:36:56 +0530

    umang first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      {/* Other components or text can go here */}
+      <iframe
+        title="Student Information Form"
+        src="https://forms.gle/TERK8Mk5uMuajEgU9"
+        width="640"
+        height="800"
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
+      >
+        Loading...
+      </iframe>
+    </div>
+  )
+}
+
+export default App
